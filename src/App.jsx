7065d740529872565104be26fd32cc390fb02645
Umesh8907 <umesh.tandon.51@gmail.com>
2024-04-27 13:21:23 +0530

    initialized project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Projects from "./Pages/Projects";
+import Services from "./Pages/Services";
+import Contact from "./Pages/Contact";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import Home from "./Pages/Home";
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
